23ac4383b7ce73af764cb0369acdc0f7bff28b48
cokepluscarbon <93948886@qq.com>
2016-09-24 23:41:20 +0800

    NetMonohavior实现

--- Assets/Script/Net/NetMonobehavior.cs
+++ Assets/Script/Net/NetMonobehavior.cs
@@ -4,24 +4,33 @@ using System.Collections.Generic;
 using System.Net.Sockets;
 using System.Net;
 using System.Threading;
+using com.ckcb.gs;
+using Google.ProtocolBuffers;
+using System.IO;
 
-public class NetMonoBehaviour : MonoBehaviour
+public class NetMonoBehavior : MonoBehaviour
 {
-    private Socket socket;
+    private TcpClient tcpClient;
+    private BinaryWriter writer;
+    private BinaryReader reader;
 
     void Awake()
     {
         DontDestroyOnLoad(gameObject);
 
-        socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
         try
         {
-            socket.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080));
+            tcpClient = new TcpClient();
+            tcpClient.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080));
+
+            writer = new BinaryWriter(tcpClient.GetStream());
+            reader = new BinaryReader(tcpClient.GetStream());
             Debug.Log("连接服务器成功");
         }
         catch
         {
-            Console.WriteLine("连接服务器失败，请按回车键退出！");
+            Debug.LogError("连接服务器失败，请按回车键退出！");
+            Destroy(gameObject);
             return;
         }
 
@@ -31,25 +40,48 @@ public class NetMonoBehaviour : MonoBehaviour
 
     public void SendWorkThreadFunction()
     {
-        try
+        while (true)
         {
-            Thread.Sleep(1000);
-        }
-        catch (Exception ex)
-        {
-            System.Console.WriteLine(ex.Message);
+            try
+            {
+                string content = "I am Unity Client!";
+                RpcRequest.Types.RequestHeader requestHeader = RpcRequest.Types.RequestHeader.CreateBuilder().SetRpcId(5).SetReqId(1).Build();
+                RpcRequest rpcRequest = RpcRequest.CreateBuilder().SetHeader(requestHeader).SetContent(ByteString.CopyFromUtf8(content)).Build();
+
+
+                byte[] byteArray = rpcRequest.ToByteArray();
+                writer.Write(byteArray.Length);
+                writer.Write(byteArray);
+                writer.Flush();
+
+                Thread.Sleep(1000);
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError(ex.Message);
+            }
         }
     }
 
     public void ReceiveWorkThreadFunction()
     {
-        try
+        while (true)
         {
-            Thread.Sleep(1000);
-        }
-        catch (Exception ex)
-        {
-            System.Console.WriteLine(ex.Message);
+            try
+            {
+                int len = reader.ReadInt32();
+                byte[] byteArray = reader.ReadBytes(len);
+
+                RpcResponse rpcResponse = RpcResponse.ParseFrom(ByteString.CopyFrom(byteArray));
+
+                System.Console.WriteLine(rpcResponse.Content.ToStringUtf8());
+
+                Thread.Sleep(1000);
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError(ex.Message);
+            }
         }
     }
 }
