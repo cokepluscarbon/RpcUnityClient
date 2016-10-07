732b82beca5e0c2a567059677d85d6879b682936
cokepluscarbon <93948886@qq.com>
2016-10-08 01:43:48 +0800

    添加事件系统：EventCenter
    1、public static void Send(string eventStr, object[] args = null)
    2、public static void AddListener(string eventStr, Action<object[]> listener)
    3、public static void RemoveListener(string eventStr, Action<object[]> listener)
    4、public static void RemoveListeners(object obj)

---
+++ Assets/Script/EventCenter/EventCenter.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class EventCenter
+{
+    private static Dictionary<string, List<Action<object[]>>> eventListeners = new Dictionary<string, List<Action<object[]>>>();
+
+    public static void Send(string eventStr, object[] args = null)
+    {
+        if (!string.IsNullOrEmpty(eventStr))
+        {
+            if (eventListeners.ContainsKey(eventStr))
+            {
+                List<Action<object[]>> listeners = eventListeners[eventStr];
+
+                Action<object[]> listener = null;
+                for (int i = listeners.Count - 1; i >= 0; i--)
+                {
+                    listener = listeners[i];
+                    if (listener != null && listener.Target != null)
+                    {
+                        listener(args);
+                    }
+                    else {
+                        listeners.RemoveAt(i);
+                    }
+                }
+            }
+        }
+    }
+
+    public static void AddListener(string eventStr, Action<object[]> listener)
+    {
+        if (!string.IsNullOrEmpty(eventStr))
+        {
+            List<Action<object[]>> listeners = GetOrCreateListeners(eventStr);
+            listeners.Add(listener);
+        }
+    }
+
+    public static void RemoveListener(string eventStr, Action<object[]> listener)
+    {
+        if (!string.IsNullOrEmpty(eventStr))
+        {
+            List<Action<object[]>> listeners = GetOrCreateListeners(eventStr);
+            listeners.Remove(listener);
+        }
+    }
+
+    public static void RemoveListeners(object obj)
+    {
+        if (obj != null)
+        {
+            foreach (List<Action<object[]>> listeners in eventListeners.Values)
+            {
+                Action<object[]> listener = null;
+                for (int i = listeners.Count - 1; i >= 0; i--)
+                {
+                    listener = listeners[i];
+                    if (listener == null || listener.Target == obj)
+                    {
+                        listeners.RemoveAt(i);
+                    }
+                }
+            }
+        }
+    }
+
+    private static List<Action<object[]>> GetOrCreateListeners(string eventStr)
+    {
+        List<Action<object[]>> listeners = null;
+        if (!eventListeners.ContainsKey(eventStr))
+        {
+            listeners = new List<Action<object[]>>();
+            eventListeners.Add(eventStr, listeners);
+        }
+        else {
+            listeners = eventListeners[eventStr];
+        }
+
+        return listeners;
+    }
+}
---
+++ Assets/Script/Test/TestEventCenter.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestEventCenter : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        EventCenter.AddListener("test_event_1", OnTestEvent1);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            EventCenter.Send("test_event_1", new object[] { 1, 2, 3, new Random() });
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            EventCenter.RemoveListener("test_event_1", OnTestEvent1);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            EventCenter.RemoveListeners(this);
+        }
+    }
+
+
+    private void OnTestEvent1(object[] args)
+    {
+        Debug.Log("test_event_1 args = " + args.Length);
+    }
+}
